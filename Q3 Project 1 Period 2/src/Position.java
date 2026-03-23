cb182200ad32f15b80813516172803d91520ff21
sdeng <sdeng@onyx>
2026-03-23 01:41:10 -0700

    Main- Layers became an instance variable
    cuz otherwise each method doesn't efficiently check the layers if i have
    to call the other helper method over and over

--- Q3 Project 1 Period 2/src/Position.java
+++ Q3 Project 1 Period 2/src/Position.java
@@ -1,84 +1,51 @@
-
-public class Position{
-	
-	private int row;
-	private int col;
-	private int layer;
-	private Position prev;
-    private static int[] goal; // Make goal static so all instances share it
-
-	public Position(int r, int c, int l) {
-		row = r;
-		col = c;
-		layer = l;
-		prev = null;
-	}
-	
-	public Position(int r, int c, int l, Position prev) {
-		row = r;
-		col = c;
-		layer = l;
-		this.prev = prev;
-	}
-	
-	public int getRow() {
-		return row;
-	}
-	public int getCol() {
-		return col;
-	}
-	public int getLayer() {
-		return layer;
-	}
+public class Position {
+    
+    private int row;
+    private int col;
+    private int layer;
+    private Position prev;
+    private static int[] goal;
+    
+    public Position(int r, int c, int l) {
+        row = r;
+        col = c;
+        layer = l;
+        prev = null;
+    }
+    
+    public Position(int r, int c, int l, Position prev) {
+        row = r;
+        col = c;
+        layer = l;
+        this.prev = prev;
+    }
+    
+    public int getRow() {
+        return row;
+    }
+    
+    public int getCol() {
+        return col;
+    }
+    
+    public int getLayer() {
+        return layer;
+    }
+    
     public static void setGoal(int[] goalCoords) {
         goal = goalCoords;
     }
     
+    public String getKey() {
+        return layer + "," + row + "," + col;
+    }
     
-	public String getKey() {
-		return layer + "," + row + "," + col;
-	}
-	
-	public Position getPrev() {
-		return prev;
-	}
-	
-	public boolean isGoal() {
-		return row == goal[0] && col == goal[1] && layer == goal[2];
-	}
-	
-	
+    public Position getPrev() {
+        return prev;
+    }
+    
+    public boolean isGoal() {
+        if (goal == null) return false;  // FIXED: Added null check
+        return row == goal[0] && col == goal[1] && layer == goal[2];
+    }
 }
-
-
-
-
-
-
-
-//   // Position class with layer information
-//
-//
-//public class Position {
-//        int row, col, layer;
-//        Position parent;
-//        
-//        Position(int row, int col, int layer) {
-//            this(row, col, layer, null);
-//        }
-//        
-//        Position(int row, int col, int layer, Position parent) {
-//            this.row = row;
-//            this.col = col;
-//            this.layer = layer;
-//            this.parent = parent;
-//        }
-//        
-//        @Override
-//        public boolean equals(Object obj) {
-//            if (this == obj) return true;
-//            if (obj == null || getClass() != obj.getClass()) return false;
-//            Position pos = (Position) obj;
-//            return row == pos.row && col == pos.col && layer == pos.layer;
-//        }
-//    }
